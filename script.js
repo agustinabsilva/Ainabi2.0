2e2719b0fd11faaa85ab785c6070b06f689e799a
Agustina Silva <agustinasilva.abs@gmail.com>
2025-05-09 19:06:03 -0300

    landing lista

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ document.addEventListener('DOMContentLoaded', function () {
         navLinks.classList.toggle('active');
     });
 });
+
+// Verifica si el botón existe antes de añadir el evento
+  document.addEventListener("DOMContentLoaded", function () {
+    const btnEnviado = document.getElementById("btnEnviar");
+
+    if (btnEnviado) {
+        btnEnviado .addEventListener("click", function (e) {
+            e.preventDefault(); // Evita el salto inmediato
+
+            Swal.fire({
+                title: "Enviado!",
+                text: "Muchas gracias por contactarte",
+                imageUrl: "./assets/capiok.png",
+                imageWidth: 400,
+                imageHeight: 400,                
+                imageAlt: "Custom image",
+                confirmButtonText: "Continuar",
+                confirmButtonColor: "#b88b66",
+               
+              });
+               // Limpia el formulario
+            const form = document.getElementById("form-contacto"); // Asegúrate de usar el ID correcto
+            if (form) {
+                form.reset();
+            }
+             
+        });
+        
+    }
+   
+});
+ 
